b236f32e47a3378961270ba9b076a26412da9d4d
OnkelDunkel <rasbalnor@gmail.com>
2019-04-19 16:27:15 +0200

    multiple assertions

--- Tests/MultipleAssertions.java
+++ Tests/MultipleAssertions.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -8,18 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MultipleAssertions {
     @Test
+    @DisplayName("8 assertions (renamed)")
     void multipleAssertions() {
-        assertFalse(false);
         assertNotEquals(0, 999);
+        assertTrue(true);
+        assertFalse(false);
         assertTimeout(Duration.ofMillis(1000), () -> {
             Thread.sleep(500);
         });
         assertDoesNotThrow(()-> {
             System.out.println("bob");
         });
-        assertNotNull(new Bank());
-        assertTrue(true);
         assertNull(null);
+        assertNotNull(new Bank());
         assertArrayEquals(new int[]{}, new int[]{});
+
+
+        assertEquals("bob", () -> {
+            String s = "bob";
+            System.out.println(s);
+            return s;
+        });
+
+
+        // Below should fail
+//        assertTimeout(Duration.ofMillis(5000), () -> {
+//            Thread.sleep(8000);
+//        });
     }
 }
